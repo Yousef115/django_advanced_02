dfa2f6c49e03c92b0ba2a6af963d85707d687214
yousef <yousef.hamadah@gmail.com>
2019-09-01 10:52:35 +0300

    Wadha and Yousef.

--- stores/models.py
+++ stores/models.py
@@ -1,11 +1,39 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Store(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    slug = models.SlugField(blank=True, unique=True)
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('detail', args=[str(self.slug)])
 
+def generate_slug(instance, new_slug=None):
+    slug = slugify(instance.name)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Store.objects.filter(slug=slug)
+    if qs.exists():
+        try:
+            int(slug[-1])
+            if "-" in slug:
+                slug_list = slug.split("-")
+                new_slug = "%s%s" % (slug[:-len(slug_list[-1])], int(slug_list[-1]) + 1)
+            else:
+                new_slug = "%s-1" % (slug)
+        except:
+            new_slug = "%s-1" % (slug)
+        return generate_slug(instance, new_slug=new_slug)
+    return slug
+
+@receiver(pre_save, sender=Store)
+def create_slug(sender, instance, *args, **kwargs):
+    instance.slug = generate_slug(instance)
